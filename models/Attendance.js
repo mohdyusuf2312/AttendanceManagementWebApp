7f9737e5291e9f681ea12698c0de095b66793d08
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-04-14 09:00:35 +0530

    Update Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const AttendanceSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    subject: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    status: { type: String, enum: ['present', 'absent', 'late'] },
-    subject: String // This will store the course/subject
+    status: { type: String, enum: ['present', 'absent', 'late'], required: true }
 });
 
 module.exports = mongoose.model('Attendance', AttendanceSchema);
