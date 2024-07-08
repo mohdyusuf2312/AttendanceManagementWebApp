f373eae8191bf1f60cf0e3b7ac3ab48fee6f45ee
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-07-08 21:23:11 +0530

    Upload scripts.js

---
+++ public/script/scripts.js
@@ -0,0 +1,14 @@
+function redirectToStudentDashboard() {
+    // Redirect to the student dashboard
+    window.location.href = "studentDashboard.html";
+}
+
+function redirectToParentDashboard() {
+    // Redirect to the parent dashboard
+    window.location.href = "parentDashboard.html";
+}
+
+function redirectToTeacherDashboard() {
+    // Redirect to the parent dashboard
+    window.location.href = "teacherDashboard.html";
+}
