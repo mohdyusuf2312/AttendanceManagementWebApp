b3c46eba451ecb68ac8ab976a515d0388b2c1f37
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-11-03 12:25:34 +0530

    require dotenv.config

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
@@ -11,17 +12,17 @@ router.post('/login', async (req, res) => {
         // Check if the user with the given enrollment number exists
         const user = await User.findOne({ enrollment_number });
         if (!user) {
-            return res.status(400).json({ error: 'Credentials do not match' });
+            return res.status(400).json({ error: 'Enrollment number do not found' });
         }
 
         // Check if the faculty number matches the stored faculty number for the user
         if (user.faculty_number !== faculty_number) {
-            return res.status(400).json({ error: 'Credentials do not match' });
+            return res.status(400).json({ error: 'Invalid faculty number' });
         }
 
         // Generate a JWT token if credentials match
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
+        res.cookie('token', token, { httpOnly: true });
         res.json({ token });
     } catch (err) {
         res.status(500).json({ error: 'An error occurred while logging in' });
