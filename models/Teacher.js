b1b28fdf9f476e4fc814ea80e8cd4bac55ad59db
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-11-03 18:55:09 +0530

    create teachers collection

---
+++ models/Teacher.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const teacherSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    department: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Teacher', teacherSchema);
