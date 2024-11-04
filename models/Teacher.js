38b04bcdcedaa8de9c69c7252d335d26f982493a
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-11-04 16:46:18 +0530

    Add token attribute in teacherSchema collection

--- models/Teacher.js
+++ models/Teacher.js
@@ -4,7 +4,9 @@ const teacherSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     department: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
 });
 
 module.exports = mongoose.model('Teacher', teacherSchema);
