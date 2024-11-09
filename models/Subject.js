2d3ccc8604455e580a3b34105d902c33e7f1560d
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-11-09 17:11:35 +0530

    Remove sub_code

--- models/Subject.js
+++ models/Subject.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const SubjectSchema = new mongoose.Schema({
-    sub_code: { type: String, required: true },
     sub_name: { type: String, required: true },
     course: { type: String, required: true }, // Course code or name, e.g., MCA
     semester: { type: String, required: true } // Semester code or name, e.g., 1, 2, etc.
